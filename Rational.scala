588b1e3107f9e3f5f322aeff291738bd8e19aaa1
Anjleen Tirkey <tirkeyanjleen@gmail.com>
2023-12-18 12:52:25 +0530

    require(condition)

--- Rational.scala
+++ Rational.scala
@@ -1,10 +1,11 @@
 class  Rational(n : Int,d : Int){
+    require(d!=0)
     override def toString = s"$n/$d"
     // println("Created"+n+"/"+d)
 }
 object  Rational_Constructor{
     def main(args : Array[String])={
-        val x = new Rational(1,3)
+        val x = new Rational(5,2)
         println(x.toString)
     }
 }
